bc9817bf646d311a6abe8d2e8e353a30cdf11d60
jhameson <jhamesonlucas22@gmail.com>
2023-05-25 23:11:01 -0300

    feat: estruturando página de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,7 +4,25 @@ import { Text, View } from 'react-native'
 import * as Styled from './styles'
 
 const SignIn = () => {
-  return <Styled.Background></Styled.Background>
+  return (
+    <Styled.Background>
+      <Styled.Container>
+        <Styled.Logo source={require('../../_assets/Logo.png')} />
+        <Styled.AreaInput>
+          <Styled.Input placeholder="Seu e-mail"></Styled.Input>
+        </Styled.AreaInput>
+        <Styled.AreaInput>
+          <Styled.Input placeholder="Sua senha"></Styled.Input>
+        </Styled.AreaInput>
+        <Styled.SubmitButton>
+          <Styled.SubmitText>Acessar</Styled.SubmitText>
+        </Styled.SubmitButton>
+        <Styled.Link>
+          <Styled.LinkText></Styled.LinkText>
+        </Styled.Link>
+      </Styled.Container>
+    </Styled.Background>
+  )
 }
 
 export default SignIn
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -4,3 +4,26 @@ export const Background = styled.View`
   flex: 1;
   background-color: #f0f4ff;
 `
+
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+export const Logo = styled.Image`
+  margin-bottom: 25px;
+`
+
+export const AreaInput = styled.View`
+  flex-direction: row;
+`
+
+export const Input = styled.TextInput`
+  background-color: #fff;
+  width: 90%;
+  font-size: 17px;
+  padding: 10px;
+  border-radius: 8px;
+  color: #121212;
+  margin-bottom: 15px;
+`
